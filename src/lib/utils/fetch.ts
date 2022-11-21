9b56bced11173fb5c0235081974b16d4af703283
pala <pala@tmapy.cz>
2022-11-21 17:08:04 +0100

    feat: show pending fetch requests

--- src/lib/utils/fetch.ts
+++ src/lib/utils/fetch.ts
@@ -1,11 +1,21 @@
+import { writable, derived } from 'svelte/store';
+
 import { createURL } from './createURL';
+
+export const pendingFetches = writable(0);
+export const isPendingFetch = derived(pendingFetches, (value) => !!value);
 
 export const getJSON = async <T = unknown>(
 	path: string,
 	args?: Record<string, string>
 ): Promise<T> => {
-	const response = await fetch(createURL(path, args));
-	return (await response.json()) as T;
+	pendingFetches.update((count) => count + 1);
+	try {
+		const response = await fetch(createURL(path, args));
+		return (await response.json()) as T;
+	} finally {
+		pendingFetches.update((count) => count - 1);
+	}
 };
 
 export type FetchDirResponse = {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
+	import { isPendingFetch, pendingFetches } from '$lib/utils/fetch';
 
 	export let data: LayoutData;
 </script>
 
 <h1>{data.cwd}</h1>
+
+{#if $isPendingFetch}
+	Loading... {$pendingFetches}
+{:else}
+	Ready
+{/if}
 
 <slot />
